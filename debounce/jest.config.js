ae8b11c2c134f83280f807fc4731284968704e11
Irine7 <irine7@yandex.ru>
2023-11-28 09:37:18 +0100

    fix: resolve bugs with jest.config

---
+++ debounce/jest.config.js
@@ -0,0 +1,7 @@
+
+export default {
+	preset: 'ts-jest',
+	testEnvironment: 'node',
+	testRegex: '(/tests/.*|(.|/)(test|spec)).(jsx?|tsx?)$',
+	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
