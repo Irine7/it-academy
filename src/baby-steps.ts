7f34e27c5b4d40c4d230a192ebc7beb7ef6811fa
Irine7 <irine7@yandex.ru>
2023-12-15 16:37:38 +0100

    refactor: add small changes to baby-steps

--- src/baby-steps.ts
+++ src/baby-steps.ts
@@ -1,17 +1,11 @@
-// Получаем аргументы из командной строки (пропускаем первые два элемента - 'node' и путь к файлу)
-// const args = process.argv.slice(2);
-
-// // Преобразуем строки в числа и вычисляем их сумму
-// export const sum = args.reduce((acc, curr) => acc + Number(curr), 0);
-
-// console.log(sum);
-
+// Преобразуем строки в числа и вычисляем их сумму
 export const sum = (args: number[]): number => {
 	return args.reduce((acc, curr) => acc + Number(curr), 0);
 };
 
 // Для вывода суммы в консоль при вызове из командной строки
 if (require.main === module) {
+	// Получаем аргументы из командной строки (пропускаем первые два элемента - 'node' и путь к файлу)
 	const args = process.argv.slice(2).map(Number);
 	console.log(sum(args));
 }
