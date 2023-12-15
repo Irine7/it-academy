767cbbf07b004ea2ed622a888c5885f2e0d9f67b
Irine7 <irine7@yandex.ru>
2023-12-15 16:34:11 +0100

    feat: add tests for baby-steps

---
+++ src/__tests__/baby-steps.test.ts
@@ -0,0 +1,23 @@
+// import { sum } from '../baby-steps';
+
+// describe('Testing if the function returns the sum of all arguments and the sum is a number', () => {
+// 	const args = [1, 2, 3];
+
+// 	it('Check if the return is a sum of given arguments and its type is a number', () => {
+// 		const res = sum(args);
+		
+// 		expect(res).toBe(6);
+// 	});
+// });
+
+import { sum } from '../baby-steps';
+
+describe('Testing if the function returns the sum of all arguments and the sum is a number', () => {
+  it('Check if the return is a sum of given arguments and its type is a number', () => {
+    const args = [1, 2, 3];
+    const res = sum(args);
+    expect(res).toBe(6);
+  });
+});
+
+
--- src/baby-steps.ts
+++ src/baby-steps.ts
@@ -1,10 +1,17 @@
 // Получаем аргументы из командной строки (пропускаем первые два элемента - 'node' и путь к файлу)
-const args = process.argv.slice(2);
+// const args = process.argv.slice(2);
 
-// Преобразуем строки в числа и вычисляем их сумму
-const sum = args.reduce((acc, curr) => acc + Number(curr), 0);
+// // Преобразуем строки в числа и вычисляем их сумму
+// export const sum = args.reduce((acc, curr) => acc + Number(curr), 0);
 
-console.log(sum);
+// console.log(sum);
 
-// Завершаем выполнение программы и возвращаем результат проверке
-process.exit();
+export const sum = (args: number[]): number => {
+  return args.reduce((acc, curr) => acc + Number(curr), 0);
+};
+
+// Для вывода суммы в консоль при вызове из командной строки
+if (require.main === module) {
+  const args = process.argv.slice(2).map(Number);
+  console.log(sum(args));
+}
