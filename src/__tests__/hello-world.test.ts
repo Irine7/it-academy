c0756e84bac228cfbc772b6513f0b4bb892df13c
Irine7 <irine7@yandex.ru>
2023-12-16 10:41:09 +0100

    feat: update hello-world test

--- src/__tests__/hello-world.test.ts
+++ src/__tests__/hello-world.test.ts
@@ -2,7 +2,6 @@ import { greeting } from '../hello-world';
 
 describe('Testing if the function returns greeting "Hello world" ', () => {
 	it('Returns the required greeting', () => {
-		const greeting = 'Hello world';
 		const res = greeting;
 		expect(res).toEqual('Hello world');
 	});
--- src/hello-world.ts
+++ src/hello-world.ts
@@ -1,2 +1,2 @@
-export const greeting = 'HELLO WORLD';
+export const greeting = 'Hello world';
 console.log(greeting);
