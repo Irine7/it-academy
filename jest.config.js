0fe2ba2b8532e216003f5cff7e7dfcfd1ab556ca
Irine7 <irine7@yandex.ru>
2023-12-17 10:44:14 +0100

    fix: update configuration files

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
 	preset: 'ts-jest',
 	testEnvironment: 'node',
-	testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
-	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-	coverageProvider: 'v8',
+	roots: ['<rootDir>/src'],
+	transform: {
+		'^.+\\.tsx?$': 'ts-jest',
+	},
 };
