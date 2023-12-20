da02d1acbc91e6322d3765ddd39172e30a75cab3
Irine7 <irine7@yandex.ru>
2023-12-20 12:41:46 +0100

    feat: add time-server.ts

---
+++ src/time-server.ts
@@ -0,0 +1,34 @@
+// Net - модуль для создания TCP сервера
+import * as net from 'net';
+
+// Получаем порт из аргументов командной строки
+const port: number = parseInt(process.argv[2], 10);
+
+// Создаем TCP сервер
+const server = net.createServer((socket) => {
+	// Функция обработки нового соединения
+
+	// Получаем текущую дату и время в формате "YYYY-MM-DD hh:mm"
+	const currentDate = new Date();
+	const formattedDate = `${currentDate.getFullYear()}-${padZero(
+		currentDate.getMonth() + 1
+	)}-${padZero(currentDate.getDate())} ${padZero(
+		currentDate.getHours()
+	)}:${padZero(currentDate.getMinutes())}\n`;
+
+	// Выводим данные в сокет
+	socket.write(formattedDate);
+
+	// Закрываем соединение
+	socket.end();
+});
+
+// Слушаем указанный порт
+server.listen(port);
+
+console.log(`TCP was run on: ${port}`);
+
+// Функция для добавления нулей к числу, если оно меньше 10
+function padZero(num: number): string {
+	return num < 10 ? `0${num}` : num.toString();
+}
