a42f84b711a45f1501457f807852a3a51dc71886
Irine7 <irine7@yandex.ru>
2023-12-21 12:10:31 +0100

    feat: update jest.config file

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,8 @@ module.exports = {
 	preset: 'ts-jest',
 	testEnvironment: 'node',
 	roots: ['<rootDir>/src'],
+	testRegex: '/__tests__/.*\\.test\\.ts$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 	transform: {
 		'^.+\\.tsx?$': 'ts-jest',
 	},
