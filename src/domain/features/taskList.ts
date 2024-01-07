487c575b0f00b3febdd2ec529beaa38e96abd3b8
Irine7 <irine7@yandex.ru>
2024-01-07 17:46:30 +0100

    feat: add taskList.ts to domain folder

---
+++ src/domain/features/taskList.ts
@@ -0,0 +1,9 @@
+import { Todo } from '../entities/todo';
+
+// Реализуем новый интерфейс для всех действий для TaskList на основе Todo
+export interface TaskList {
+	addTask(task: Todo): Todo;
+	getTasks(): Todo[];
+	removeTask(id: number): boolean;
+	changeStatus(id: number): Todo;
+}
