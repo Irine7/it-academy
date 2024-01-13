833d52c0c756386650dbc08aafbf3c76c259e2b8
Irine7 <irine7@yandex.ru>
2024-01-13 17:56:52 +0100

    feat: add infrastructure/http file

---
+++ src/infrastructure/http/app.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import { basicAuthMiddleware } from '../middlewares/basicAuthMiddleware';
+import { cacheMiddleware } from '../middlewares/cacheMiddleware';
+import { corsMiddleware } from '../middlewares/corsMiddleware';
+import { jsonMiddleware } from '../middlewares/jsonMiddleware';
+import { routerTodoApp } from '../../user-interface/http/appControllers';
+
+export const app = express();
+
+app.use(basicAuthMiddleware);
+app.use(cacheMiddleware);
+app.use(corsMiddleware);
+app.use(jsonMiddleware);
+
+app.use('/api/todo', routerTodoApp);
